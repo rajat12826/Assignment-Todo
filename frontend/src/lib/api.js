dfe8c5f6972f3712df17fc605155e843e727ec5d
rajat12826 <pariharrajat078@gmail.com>
2025-02-21 22:47:34 +0530

    f

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,14 +5,4 @@ const api = axios.create({
 });
 
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('userToken'); 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; 
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
 
-export default api;
